9e91559bb1a701dea7fc8ffef7ca79eaadf802fd
lakshmanui <lakshmankumar@outlook.com>
2016-02-24 23:12:15 -0500

    first commit

---
+++ cont/ctrl.js
@@ -0,0 +1,66 @@
+function register($scope){
+	$scope.users=[
+       {
+            firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        },
+        { 
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+
+        },
+        {
+        	firstname:"kumar",
+            lastname:"kumar",
+            email:"kumar@gmail.com"
+        }
+
+
+	];
+	
+
+}
+app.controller('registerCtrl',['$scope',register]);
